44254a6f0fe6485f6e2f3903bbb89b213a9e4d5e
Taimoor Malik <66994297+TaimoorMalik-Sunny@users.noreply.github.com>
2023-08-24 10:08:36 +0000

    Show images on gallery page

--- src/app/gallary/page.tsx
+++ src/app/gallary/page.tsx
@@ -1,31 +1,41 @@
-'use client'
-import { CldUploadButton } from "next-cloudinary"
-import { UploadResult } from "../page"
-import { Button } from "@/components/ui/button"
+import { CldImage } from 'next-cloudinary'
+import Upload_button from './upload_button'
+import cloudinary from "cloudinary"
+import CloudinaryImage from './cloudinary_images'
 
-export default function GallaryPage(){
+type SearchResult = {
+  public_id: string
+}
+
+export default async function GallaryPage(){
+
+  const results = await cloudinary.v2.search
+  .expression("resource_type:image")
+  .sort_by('created_at','desc')
+  .max_results(10)
+  .execute() as {resources:SearchResult[]}
+   
+  
+
   return(
     <section>
-      <div className="flex justify-between">
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
         <h1 className="text-4xl font-bold" >Gallery</h1>
-        <Button asChild>  
-
-        <div className="flex gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-</svg>
-
+       <Upload_button/>
+       </div>
+      <div className="grid grid-cols-4 gap-4 ">
+       {results.resources.map(result=>(
        
-
-        <CldUploadButton 
-          onUpload = {(result: UploadResult)=>{
-          // setImageId(result.info.public_id)
-          }}
-          uploadPreset="fi6ysuag" />
-           </div>
-          
-          </Button>
-
+          <CloudinaryImage
+          key={result.public_id}
+          src={result.public_id}
+          width="400"
+          height="300"
+          alt="an image somthing "
+          />
+        ))}
+       </div>
       </div>
     </section>
   )
